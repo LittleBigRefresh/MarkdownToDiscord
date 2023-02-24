450359dc538e32106dd56e2c16361491a7cbfc6e
jvyden <jvyden@jvyden.xyz>
2023-02-24 01:36:37 -0500

    Markdown directory, do everything through env vars

--- MarkdownToDiscord/Program.cs
+++ MarkdownToDiscord/Program.cs
@@ -3,41 +3,29 @@ using Discord.Rest;
 using MarkdownToDiscord;
 using MarkdownToDiscord.Elements;
 
-MarkdownParser parser = new(File.ReadAllLines("test.md").ToList());
+string? mdPath = Environment.GetEnvironmentVariable("MARKDOWN_FILES_DIR");
+string? token = Environment.GetEnvironmentVariable("DISCORD_TOKEN");
 
-string token = File.ReadAllText("token.txt");
+if (token == null) throw new InvalidOperationException("Cannot proceed without a Discord token");
+if (mdPath == null) throw new InvalidOperationException("Cannot proceed without a markdown directory");
+
+string[] files = Directory.GetFiles(mdPath);
 
 DiscordRestClient client = new(new DiscordRestConfig
 {
     LogLevel = LogSeverity.Debug,
 });
 
-#pragma warning disable CS1998
-client.Log += async message => Console.WriteLine(message);
-#pragma warning restore CS1998
-
-await client.LoginAsync(TokenType.Bot, token);
-
-IChannel? channel = await client.GetChannelAsync(1078539918642516018);
-if (channel == null) throw new ArgumentNullException(nameof(channel));
-
-if (channel is IMessageChannel messageChannel)
+async Task PostMarkdownFile(IMessageChannel channel, MarkdownParser parser, HttpClient httpClient)
 {
-    await foreach (IReadOnlyCollection<IMessage> messages in messageChannel.GetMessagesAsync(50))
-    foreach (IMessage message in messages)
-        await message.DeleteAsync();
-
-    using HttpClient httpClient = new();
-    httpClient.DefaultRequestHeaders.Add("Accept", "image/*");
-    
     string buffer = string.Empty;
     foreach (IMarkdownElement element in parser.Parse())
     {
         switch (element)
         {
             case HeaderMarkdownElement headerElement:
-                if (!string.IsNullOrWhiteSpace(buffer)) await messageChannel.SendMessageAsync(buffer);
-                buffer = $"**{headerElement.Text}**\n\n";
+                if (!string.IsNullOrWhiteSpace(buffer)) await channel.SendMessageAsync(buffer);
+                buffer = $"**{headerElement.Text}**\n";
                 break;
             case TextMarkdownElement textElement:
                 buffer += textElement.Text + '\n';
@@ -47,7 +35,7 @@ if (channel is IMessageChannel messageChannel)
                 FileAttachment attachment = new(stream, "image.png", imageElement.AltText);
                 
                 // it's okay to not check for length here, we're free to send attachments with no message body
-                await messageChannel.SendFileAsync(attachment, buffer);
+                await channel.SendFileAsync(attachment, buffer);
                 buffer = string.Empty;
                 break;
         }
@@ -55,7 +43,36 @@ if (channel is IMessageChannel messageChannel)
 
     if (!string.IsNullOrWhiteSpace(buffer))
     {
-        await messageChannel.SendMessageAsync(buffer.TrimEnd());
+        await channel.SendMessageAsync(buffer.TrimEnd());
+    }
+}
+
+#pragma warning disable CS1998
+client.Log += async message => Console.WriteLine(message);
+#pragma warning restore CS1998
+
+await client.LoginAsync(TokenType.Bot, token);
+
+using HttpClient httpClient = new();
+httpClient.DefaultRequestHeaders.Add("Accept", "image/*");
+
+foreach (string file in files)
+{
+    if(!file.EndsWith(".md")) continue;
+    Console.WriteLine("Processing file " + file);
+    
+    // TODO: parse md file's front matter for channel id
+    List<string> lines = (await File.ReadAllLinesAsync(file)).ToList();
+    IChannel? channel = await client.GetChannelAsync(1078539918642516018);
+    if (channel == null) throw new InvalidOperationException("Could not find channel by id {id}");
+
+    if (channel is IMessageChannel messageChannel)
+    {
+        await foreach (IReadOnlyCollection<IMessage> messages in messageChannel.GetMessagesAsync(50))
+        foreach (IMessage message in messages)
+            await message.DeleteAsync();
+
+        await PostMarkdownFile(messageChannel, new MarkdownParser(lines), httpClient);
     }
 }
 
