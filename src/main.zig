1d82e888ff1b1dd2e2dfd0e5c086a85e99c5dc16
jvyden <jvyden@jvyden.xyz>
2023-05-05 00:22:10 -0400

    Discord REST requests

--- src/main.zig
+++ src/main.zig
@@ -2,12 +2,15 @@ const std = @import("std");
 const requests = @import("requests.zig");
 
 // const gatewayUrl = std.Uri.parse("https://discord.com/api/gateway") catch unreachable;
-const gatewayUrl = std.Uri.parse("http://localhost:10060") catch unreachable;
+// const gatewayUrl = std.Uri.parse("http://localhost:10060") catch unreachable;
+const gatewayUrl = std.Uri.parse("https://discord.com/api/v10/channels/1049225653398016080") catch unreachable;
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var allocator = gpa.allocator();
 
-    var data = try requests.post(gatewayUrl, "a", allocator);
+    requests.token = std.os.getenv("DISCORD_TOKEN");
+
+    var data = try requests.get(gatewayUrl, allocator);
     defer allocator.free(data);
 }
--- src/requests.zig
+++ src/requests.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 
-const RequestError = error{ NoResponseData, MismatchedContentLength };
+const RequestError = error{ NoResponseData, MismatchedContentLength, MissingBotToken };
+
+pub var token: ?[]const u8 = null;
 
 fn request(comptime method: std.http.Method, comptime url: std.Uri, body: []const u8, allocator: std.mem.Allocator) ![]u8 {
     var client: std.http.Client = .{ .allocator = allocator };
@@ -9,33 +11,39 @@ fn request(comptime method: std.http.Method, comptime url: std.Uri, body: []cons
     var req = try client.request(method, url, .{ .allocator = allocator }, .{});
     defer req.deinit();
 
-    var contentLengthHeaderValue = try std.fmt.allocPrint(allocator, "{d}", .{body.len});
+    if (body.len > 0) {
+        var contentLengthHeaderValue = try std.fmt.allocPrint(allocator, "{d}", .{body.len});
+        defer allocator.free(contentLengthHeaderValue);
+        try req.headers.append("Content-Length", contentLengthHeaderValue);
+    }
 
-    try req.headers.append("Content-Length", contentLengthHeaderValue);
+    if (token == null) {
+        return RequestError.MissingBotToken;
+    }
+
+    var authorizationValue = try std.fmt.allocPrint(allocator, "Bot {?s}", .{token});
+    defer allocator.free(authorizationValue);
+    try req.headers.append("Authorization", authorizationValue);
+
+    // Comply with discord user-agent regulation: https://discord.com/developers/docs/reference#user-agent-user-agent-example
+    try req.headers.append("User-Agent", "DiscordBot (https://github.com/LittleBigRefresh/MarkdownToDiscord, 0.0.0)");
+    try req.headers.append("Accept", "application/json");
+
+    // Fire off the request, write the body
     try req.start();
     try req.writeAll(body);
     try req.wait();
 
-    if (req.response.content_length == null) {
-        req.response.content_length = 0;
-    }
+    std.debug.print("Response status: {d}, content length is {?d} bytes\n", .{ req.response.status, req.response.content_length });
 
-    std.debug.print("Response status: {d}, bytes received: {?d}\n", .{ req.response.status, req.response.content_length });
-
-    var data: []u8 = try allocator.alloc(u8, req.response.content_length.?);
-    var read = try req.readAll(data);
-
-    if (read != req.response.content_length) {
-        return error.MismatchedContentLength;
-    }
-
+    var data: []u8 = try req.reader().readAllAlloc(allocator, 16384);
     std.debug.print("Response: {s}\n", .{data});
 
     return data;
 }
 
 pub fn get(comptime url: std.Uri, allocator: std.mem.Allocator) ![]const u8 {
-    return try request(.GET, url, .{}, allocator);
+    return try request(.GET, url, &[0]u8{}, allocator);
 }
 
 pub fn post(comptime url: std.Uri, body: []const u8, allocator: std.mem.Allocator) ![]const u8 {
