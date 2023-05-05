dfdf8d06ed616456270304eb80ad3755131d0704
jvyden <jvyden@jvyden.xyz>
2023-05-05 01:38:05 -0400

    Add general getObject() function to fetch something from the API

--- src/discord.zig
+++ src/discord.zig
@@ -25,17 +25,29 @@ pub fn setToken(token: ?[]const u8, allocator: std.mem.Allocator) !void {
     requests.authorization = authorizationValue;
 }
 
-pub fn getChannel(id: u64, allocator: std.mem.Allocator) !DiscordChannel {
-    var urlString = try std.fmt.allocPrint(allocator, "{s}/channels/{d}", .{ apiBaseUrl, id });
+fn getObject(comptime T: type, comptime endpointFmt: []const u8, args: anytype, allocator: std.mem.Allocator) !T {
+    if (endpointFmt[0] != '/') {
+        @compileError("Endpoint URL must start with a slash.");
+    }
+
+    var endpointUrlString = try std.fmt.allocPrint(allocator, endpointFmt, args);
+    defer allocator.free(endpointUrlString);
+
+    var urlString = try std.fmt.allocPrint(allocator, "{s}{s}", .{ apiBaseUrl, endpointUrlString });
     defer allocator.free(urlString);
 
-    var channelData = try requests.get(try std.Uri.parse(urlString), allocator);
-    defer allocator.free(channelData); // yes, we free this here - it's not needed after we parse json
+    // now that we've constructed the url, lets fire a request and try to parse an object from it
+    var data = try requests.get(try std.Uri.parse(urlString), allocator);
+    defer allocator.free(data); // yes, we free this here - it's not needed after we parse json
 
     const options: std.json.ParseOptions = .{ .ignore_unknown_fields = true, .allocator = allocator };
 
-    var channelStream = std.json.TokenStream.init(channelData);
-    const channel = try std.json.parse(DiscordChannel, &channelStream, options);
+    var jsonStream = std.json.TokenStream.init(data);
+    const object = try std.json.parse(T, &jsonStream, options);
 
-    return channel;
+    return object;
+}
+
+pub fn getChannel(id: u64, allocator: std.mem.Allocator) !DiscordChannel {
+    return getObject(DiscordChannel, "/channels/{d}", .{id}, allocator);
 }
